c646dd036ac17b0936687e144877d9acefca57f4
Md shahnebaj <mdshahnebaj649@gmail.com>
2025-06-23 19:42:58 +0530

    complete calculation can do

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,17 @@ import { evaluate } from "mathjs";
 function App() {
   let [result, setresult] = useState("");
   let [isResult, setIsResult] = useState(false);
-
+  
+  
   let buttonclick = (value) => {
-    if (value === "CE") {
+    if (value === "Clear") {
       setresult("");
       setIsResult(false);
     } else if (value === "=") {
       try {
+        if ((result.match(/\(/g) || []).length > (result.match(/\)/g) || []).length) {
+          result += ")";
+        }
         let total = evaluate(result);
         if (total === Infinity || total === -Infinity) {
           setresult("Infinity");
@@ -25,7 +29,10 @@ function App() {
         setresult("Error");
         setIsResult(true);
       }
-    } else if (value === "*" || value === "/" || value === "+" || value === "-") {
+    } else if (value === "x") {
+      setresult(result + "*");
+      setIsResult(false);
+    } else if (value === "/" || value === "+" || value === "-") {
       setresult(result + value);
       setIsResult(false);
     } else {
@@ -37,11 +44,10 @@ function App() {
       }
     }
   };
-
   return (
     <>
       <div className="flex justify-center items-center bg-slate-500 h-[100vh] w-[100vw]">
-        <div className="shadow-current shadow-2xl drop-shadow-2xl place-content-center place-items-center h-[500px] w-[300px] border rounded-lg ">
+        <div className="shadow-current shadow-2xl drop-shadow-2xl place-content-center place-items-center  border rounded-lg ">
           <Input result={result} />
           <Button buttonclick={buttonclick} />
         </div>
@@ -49,5 +55,4 @@ function App() {
     </>
   );
 }
-
 export default App;
--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,14 +1,69 @@
 function Button({buttonclick}) {
-  const num = ["%","CE","(",")",7,8,9,"*",4,5,6,"+",1,2,3,"-","00",0,"=","/",];
+  const num1 = ["sin(", "cos(", "tan(", "sqrt("];
+  const num2 = ["Clear","(",")","%"];
+  // const num4 = ["4","5","6","1","2","3","00","0","="];
+  const num3 = ["7","8","9","x"];
+  const num4 = ["4","5","6","+"];
+  const num5 = ["1","2","3","-"];
+  const num6 = ["00","0","=","/"];
+  // const num5 = ["+","-","x","/"];
   return (
     <div>
-         <div className="grid grid-flow-row grid-cols-4 justify-center items-center bg-inherit   h-96 w-72 gap-3 p-3 ">
+         <div className=" bg-inherit p-2">
+            <div className="grid grid-flow-row grid-cols-4 justify-center items-center bg-inherit   ">
             {
-              num.map((n)=> (
-                <div className="shadow-lg shadow-current  bg-slate-500 border border-spacing-11 border-neutral-300 py-2 rounded-full flex text-[25px] justify-center items-center text-blue-700 cursor-pointer " 
+              num1.map((n)=> (
+                <div className="shadow-lg shadow-current  bg-slate-500 border border-spacing-11
+                 border-neutral-300 w-20 h-16 rounded-3xl flex text-[1.5rem] justify-center items-center text-blue-700 cursor-pointer hover:bg-slate-400 " 
                 key={n} onClick={()=> buttonclick(n)}>{n}</div>
               ))
             }
+            </div>
+            <div className="grid grid-flow-row grid-cols-4 justify-center items-center bg-inherit   ">
+            {
+              num2.map((n)=> (
+                <div className="shadow-lg shadow-current  bg-slate-500 border border-spacing-11
+                 border-neutral-300 w-20 h-16 rounded-3xl flex text-[1.5rem] justify-center items-center text-blue-700 cursor-pointer hover:bg-slate-400 " 
+                key={n} onClick={()=> buttonclick(n)}>{n}</div>
+              ))
+            }
+            </div>
+            <div className="grid grid-flow-row grid-cols-4 justify-center items-center bg-inherit   ">
+            {
+              num3.map((n)=> (
+                <div className="shadow-lg shadow-current  bg-slate-500 border border-spacing-11
+                 border-neutral-300 w-20 h-16 rounded-3xl flex text-[1.5rem] justify-center items-center text-blue-700 cursor-pointer hover:bg-slate-400 " 
+                key={n} onClick={()=> buttonclick(n)}>{n}</div>
+              ))
+            }
+            </div>
+            <div className="grid grid-flow-row grid-cols-4 justify-center items-center bg-inherit   ">
+            {
+              num4.map((n)=> (
+                <div className="shadow-lg shadow-current  bg-slate-500 border border-spacing-11
+                 border-neutral-300 w-20 h-16 rounded-3xl flex text-[1.5rem] justify-center items-center text-blue-700 cursor-pointer hover:bg-slate-400 " 
+                key={n} onClick={()=> buttonclick(n)}>{n}</div>
+              ))
+            }
+            </div>
+            <div className="grid grid-flow-row grid-cols-4 justify-center items-center bg-inherit   ">
+            {
+              num5.map((n)=> (
+                <div className="shadow-lg shadow-current  bg-slate-500 border border-spacing-11
+                 border-neutral-300 w-20 h-16 rounded-3xl flex text-[1.5rem] justify-center items-center text-blue-700 cursor-pointer hover:bg-slate-400 " 
+                key={n} onClick={()=> buttonclick(n)}>{n}</div>
+              ))
+            }
+            </div>
+            <div className="grid grid-flow-row grid-cols-4 justify-center items-center bg-inherit   ">
+            {
+              num6.map((n)=> (
+                <div className="shadow-lg shadow-current  bg-slate-500 border border-spacing-11
+                 border-neutral-300 w-20 h-16 rounded-3xl flex text-[1.5rem] justify-center items-center text-blue-700 cursor-pointer hover:bg-slate-400 " 
+                key={n} onClick={()=> buttonclick(n)}>{n}</div>
+              ))
+            }
+            </div>
             </div>
     </div>
   )
